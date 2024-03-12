d5c83042a6cb9fd3e4cf38af8af58a41bfd85788
unknown <vinicius.moraes.030805@gmail.com>
2024-03-12 17:23:44 -0300

    update novo exs

--- Lt01_EstDecisao/src/Lt01_EstDec28.java
+++ Lt01_EstDecisao/src/Lt01_EstDec28.java
@@ -13,14 +13,43 @@
 import javax.swing.*;
 
 public class Lt01_EstDec28 {
+
+
     public static void main(String[] args) {
         Double precoAtual, precoNovo, vendaMensal;
+        int cod;
 
         precoAtual = Double.parseDouble(JOptionPane.showInputDialog("Qual o preco atual do produto"));
         vendaMensal = Double.parseDouble(JOptionPane.showInputDialog("Qual a media de vendo do produto"));
 
-        //fazer com switch casae
-        
+        if(vendaMensal < 500 && precoAtual <30){
+            cod = 1;
+        }
+        else if(vendaMensal >= 1000 && precoAtual >= 80){
+             cod = 2;
+        }
+        else if(vendaMensal >= 500 && vendaMensal < 1000 && precoAtual >= 30 && precoAtual < 80){
+            cod = 3;
+        }else{
+            cod = 4;
+        }
+
+        switch (cod) {
+            case 1: precoNovo = (precoAtual + (precoAtual * 0.10));
+                    JOptionPane.showMessageDialog(null,"Novo Preco "+ precoNovo);
+                break;
+
+            case 2: precoNovo = (precoAtual - (precoAtual * 0.05));
+                JOptionPane.showMessageDialog(null,"Novo Preco "+ precoNovo);
+                break;
+
+            case 3: precoNovo = (precoAtual + (precoAtual * 0.15));
+                    JOptionPane.showMessageDialog(null,"Novo Preco "+ precoNovo);
+                break;
+
+            default: JOptionPane.showMessageDialog(null,"`Preco inalterao " + precoAtual);
+                break;
+        }
 
 
     }
---
+++ lt01_EstRepeticao/src/Lt01_EstRep31.java
@@ -0,0 +1,14 @@
+//Calcule e mostre o quadrado dos números entre 10 e 150
+
+
+import javax.swing.*;
+public class Lt01_EstRep31 {
+    public static void main(String[] args) {
+        //int num;
+
+        for(int cta = 10; cta >= 15; cta++){
+            JOptionPane.showMessageDialog(null,"O quadrado é "+ cta*cta);;
+        }
+
+    }
+}
