a30fed512c3f95ee385cae14e7820121f79d9809
Vini Crepaldi <Vinicius de Moraes Crepaldi>
2024-03-21 21:57:40 -0300

    Fim da Repeticao

--- lt01_EstRepeticao/src/Lt01_EstRep40.java
+++ lt01_EstRepeticao/src/Lt01_EstRep40.java
@@ -6,43 +6,13 @@ import javax.swing.*;
 public class Lt01_EstRep40 {
     
     public static void main(String[] args) {
-<<<<<<< HEAD
-        double n1,n2;
-        int teste = 0;
-=======
         int n1, n2, primo = 0;
 
         n1 =  Integer.parseInt(JOptionPane.showInputDialog("Primeiro numero "));
         n2  =  Integer.parseInt(JOptionPane.showInputDialog("segundo  numero "));
->>>>>>> f4bd492606713c2e92503ea03e33b5c464a85f64
 
-        n1 = Double.parseDouble(JOptionPane.showInputDialog("Primeiro numero "));
-        n2 = Double.parseDouble(JOptionPane.showInputDialog("Segundo numero "));
 
         if(n1>n2){
-<<<<<<< HEAD
-
-            for(double cta = (n1-1); cta > n2;cta--){// fila n2, num, num... n1
-                //System.out.println(cta);
-
-                for(double ind = (cta - 1);ind > 1; ind-- ){
-                    if(cta%ind == 0){
-                        teste = 1;
-                        ind = 1;
-                    }    
-                
-                    
-                }
-                switch (teste) {
-                    case 0: System.out.println(cta/*  Nao e primo"*/);
-                        
-                        break;
-                
-                    default: //System.out.println(cta+" E primo");
-                        
-                        break;
-                }
-=======
             for(int cta = (n1-1); cta > n2; cta--){
                 primo = 1;
 
@@ -89,7 +59,6 @@ public class Lt01_EstRep40 {
                         break;
                 }
 
->>>>>>> f4bd492606713c2e92503ea03e33b5c464a85f64
             }
         }
     }
