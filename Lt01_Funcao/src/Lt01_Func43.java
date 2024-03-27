4adf2645d7cf8c3876c31aef58aa47cfdadc49bf
Vini Crepaldi <Vinicius de Moraes Crepaldi>
2024-03-27 11:16:34 -0300

    Fim Estrutura de Repeticao

--- Lt01_Funcao/src/Lt01_Func43.java
+++ Lt01_Funcao/src/Lt01_Func43.java
@@ -4,9 +4,25 @@
  * e cresce 2 cm ao ano.
  */
 
-
-
+import javax.swing.JOptionPane;
 
 public class Lt01_Func43 {
     
+    static int calculaIdade(int ana, int maria){
+        int ano = 0;
+
+        while(ana<maria){
+            ana += 3;
+            maria += 2;
+            ano++;
+        }
+        return ano;
+    }
+    public static void main(String[] args) {
+        int res;
+
+        res = calculaIdade(110, 150);
+
+        JOptionPane.showMessageDialog(null,"Total de anos: "+res);
+    }
 }   
